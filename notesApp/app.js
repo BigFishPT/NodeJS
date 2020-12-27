5bdf8038b3321d11a7221e095323b103e0291e1e
Joao Machado <1161884@isep.ipp.pt>
2020-12-27 00:04:25 +0000

    added chalk and yargs to the project. Added add,remove,list & read commands by terminal

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,7 +1,39 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const myNotes = getNotes()
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note')
+    }
+})
 
-console.log(myNotes)
-console.log(validator.isEmail('teste'))
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function(){
+        console.log('Listing all notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+
+
+
+console.log(yargs.argv)
