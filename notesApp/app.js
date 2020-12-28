97c240fde691d4183d72a4b6782edabb0b086db2
Joao Machado <1161884@isep.ipp.pt>
2020-12-28 08:22:15 +0000

    added methods to remove, list and read notes from a file

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,39 +1,70 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
+/* Creates argument command for 
+adding a note */
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Description of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
 })
-
+/* Creates argument command for 
+removing a note */
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function(){
-        console.log('Removing a note')
+    builder: {
+        title: {
+            describe: 'Title of the Note to remove',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
 })
-
+/* Creates argument command for 
+listing all notes */
 yargs.command({
     command: 'list',
     describe: 'List all notes',
-    handler: function(){
-        console.log('Listing all notes')
+    handler: function () {
+        notes.listAllNotes()
     }
 })
-
+/* Creates argument command for 
+reading a specific  note */
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function(){
-        console.log('Reading a note')
+    builder: {
+        title: {
+            describe: 'title of the note to read',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.readNote(argv.title)
     }
 })
 
 
+yargs.parse()
 
-console.log(yargs.argv)
--- notesApp/notes.js
+++ notesApp/notes.js
@@ -1,4 +1,87 @@
-const getNotes = function(){
-    return 'Your notes...'
+/* Imports */
+const fs = require('fs')
+const chalk = require('chalk')
+
+/* Adds a Note to JSON file
+Validates if note already exists */
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title already exists!'))
+    }
 }
-module.exports = getNotes
+/* Removes a note by title
+Validates if note exists */
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title != title
+    })
+    if (notes.length != notesToKeep.length) {
+        console.log(chalk.green('Note deleted!'))
+    } else {
+        console.log(chalk.red.inverse('No note with that title!'))
+    }
+    saveNotes(notesToKeep)
+}
+/* Lists all Notes
+Validates if note exists */
+const listAllNotes = function () {
+    const notes = loadNotes()
+    if (notes.length < 1) {
+        console.log(chalk.red.inverse('There are no notes!'))
+    } else
+        while (notes.length > 0) {
+            noteToPrint = notes.pop()
+            console.log(noteToPrint)
+        }
+}
+
+/* Reades a specific a note by title
+Validates if note exists */
+const readNote = function (title) {
+    const notes = loadNotes()
+    const myNote = notes.filter(function (note) {
+        return note.title === title
+    })
+    if (myNote.length < 1) {
+        console.log(chalk.red.inverse('No note with that title!'))
+    } else {
+        noteToPrint = myNote.pop()
+        console.log('Title: ' + noteToPrint.title + ' \nBody: ' + noteToPrint.body)
+    }
+}
+/* Sabes a array of notes into JSON file */
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+/* Reads and Parses Notes in JSON format from file 
+and returns an array of Notes */
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+/* Exports all the required functions from script */
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listAllNotes: listAllNotes,
+    readNote: readNote
+}
