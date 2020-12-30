6ad460c9cf6306ea8cf0733b9999efa32af05d57
Joao Machado <1161884@isep.ipp.pt>
2020-12-30 21:40:09 +0100

    refractor code to use shorthand and destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,16 +8,15 @@ const address = process.argv[2]
 if (!address) {
     console.log('Please provide an address')
 } else {
-    geoCode(address, (error, data) => {
+    geoCode(address, (error, {location,latitude,longitude} = {}) => {
         if (error) {
             return console.log(error)
         }
-        const city = data.location
-        forecast(data.latitude, data.longitude, (error, forcastData) => {
+        forecast(latitude,longitude, (error, forcastData) => {
             if (error) {
-                return console.log('Error', error)
+                return console.log('Error', error) 
             }
-            console.log(data, forcastData)
+            console.log(location, forcastData)
         })
     })
 }
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,16 +2,16 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5d4c3e50f35dd480c0583bfc0de597ed&query=' + latitude + ',' + longitude
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to the API', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('No matches found for thoose coordinates', undefined)
         } else {
             callback(undefined, {
-                description: response.body.current.weather_descriptions[0],
-                temp: response.body.current.temperature,
-                precip: response.body.current.precip
+                description: body.current.weather_descriptions[0],
+                temp: body.current.temperature,
+                precip: body.current.precip
             })
         }
     })
