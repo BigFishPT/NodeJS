a401c87430b08b1a4e8c3a091d97130efad775db
Joao Machado <1161884@isep.ipp.pt>
2020-12-30 21:01:02 +0100

    chained callback. Command line argument for the city name

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,11 +2,22 @@ const request = require('request')
 const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
 
-geoCode('lodz', (error, data) => {
-    console.log(error)
-    console.log(data)
-})
-forecast(51.75949, 19.45851, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geoCode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+        const city = data.location
+        forecast(data.latitude, data.longitude, (error, forcastData) => {
+            if (error) {
+                return console.log('Error', error)
+            }
+            console.log(data, forcastData)
+        })
+    })
+}
