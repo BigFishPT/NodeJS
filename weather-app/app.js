264e67287a04047beaa4211ed5a06c73b51954b2
Joao Machado <1161884@isep.ipp.pt>
2020-12-30 20:48:55 +0100

    added method to get coordinates from a city name string & get forecast from coordinates

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,2 +1,12 @@
 const request = require('request')
+const geoCode = require('./utils/geoCode')
+const forecast = require('./utils/forecast')
 
+geoCode('lodz', (error, data) => {
+    console.log(error)
+    console.log(data)
+})
+forecast(51.75949, 19.45851, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
